f9885ec1abb48799cc04be6bbdea11ce1ad5098c
brittany klose <brittany.klose@yahoo.com>
2022-01-22 17:42:01 -0500

    working on icons yay

--- script2.js
+++ script2.js
@@ -15,6 +15,13 @@ var cloudyIcon=document.getElementById("cloudy")
 var sunnyIcon=document.getElementById("sunny")
 var overcastIcon=document.getElementById("overcast")
 var rainyIcon=document.getElementById("rain")
+var snowIcon=document.getElementById("snow")
+//var icon0=document.getElementById("i0")
+//var icon1=document.getElementById("i1")
+//var icon2=document.getElementById("i2")
+//var icon3=document.getElementById("i3")
+//var icon4=document.getElementById("i4")
+//var icon5=document.getElementById("i5")
 
 
 
@@ -23,7 +30,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city_Name + "&appid
 ).then(function (response) {
     return response.json()
 }).then(function (data) {
-    console.log(data);
+    //console.log(data);
     var lat = data.coord.lat
     var lon = data.coord.lon
    
@@ -32,15 +39,84 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city_Name + "&appid
         return response.json()
     }).then(function (data) {
         console.log(data); 
+// If statments for current day icons
     if (data.current.weather[0].main==="Clear"){
-           $("#i0").html(sunnyIcon)
+         $("#i0").html(sunnyIcon)
         }
-    if(data.daily[0].weather[0].main==="Rain"){
-        $("#i0").html(rainyIcon)
-    } 
-    if(data.daily[0].weather[0].main==="Clouds"){
-        $("#i0").html(cloudyIcon)
-    }
+    if(data.current.weather[0].main==="Rain"){
+         $("#i0").html(rainyIcon)
+        } 
+    if(data.current.weather[0].main==="Clouds"){
+         $("#i0").html(cloudyIcon)
+        }  
+    if(data.current.weather[0].main==="Snow"){
+         $("#i0").html(snowIcon)
+        }    
+// If statements for day 1 icons
+    if(data.daily[1].weather[0].main==="Clear"){
+         $("#i1").html(sunnyIcon)
+        }
+     if(data.daily[1].weather[0].main==="Rain"){
+         $("#i1").html(rainyIcon)
+        }
+     if(data.daily[1].weather[0].main==="Clouds"){
+         $("#i1").html(cloudyIcon)
+        }
+     if(data.daily[1].weather[0].main==="Snow"){
+         $("#i1").html(snowIcon)
+        }
+// If statements for day 2 icons
+    if(data.daily[2].weather[0].main==="Clear"){
+         $("#i2").html(sunnyIcon)
+        }
+    if(data.daily[2].weather[0].main==="Rain"){
+         $("#i2").html(rainyIcon)
+        }
+    if(data.daily[2].weather[0].main==="Clouds"){
+         $("#i2").html(cloudyIcon)
+        }
+    if(data.daily[2].weather[0].main==="Snow"){
+         $("#i2").html(snowIcon)
+        }
+//If statments for day 3 Icons
+    if(data.daily[3].weather[0].main==="Clear"){
+         $("#i3").html(sunnyIcon)
+        }
+    if(data.daily[3].weather[0].main==="Rain"){
+         $("#i3").html(rainyIcon)
+        }
+    if(data.daily[3].weather[0].main==="Clouds"){
+         $("#i3").html(cloudyIcon)
+        }
+    if(data.daily[3].weather[0].main==="Snow"){
+         $("#i3").html(snowIcon)
+        }
+//If statments for day 4 Icons
+    if(data.daily[4].weather[0].main==="Clear"){
+         $("#i4").html(sunnyIcon)
+        }
+    if(data.daily[4].weather[0].main==="Rain"){
+         $("#i4").html(rainyIcon)
+         }
+    if(data.daily[4].weather[0].main==="Clouds"){
+         $("#i4").html(cloudyIcon)
+        }
+    if(data.daily[4].weather[0].main==="Snow"){
+         $("#i4").html(snowIcon)
+        }
+//If statments for day 5 Icons
+    if(data.daily[5].weather[0].main==="Clear"){
+         $("#i5").html(sunnyIcon)
+        }
+    if(data.daily[5].weather[0].main==="Rain"){
+         $("#i5").html(rainyIcon)
+        }
+    if(data.daily[5].weather[0].main==="Clouds"){
+         $("#i5").html(cloudyIcon)
+        }
+    if(data.daily[5].weather[0].main==="Snow"){
+         $("#i5").html(snowIcon)
+        }
 
     var temp = document.getElementById("temp")
         temp.textContent="Temp: " + ((data.current.temp-273.15)*1.8+32).toFixed(2);
@@ -49,12 +125,12 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city_Name + "&appid
     var humidity = document.getElementById("humidity")
         humidity.textContent="Humidity: " + data.current.humidity
     var uvi = document.getElementById("uvi")
-        uvi.textContent="UVI: " + data.daily[0].uvi
-        if (uvi<=2.00){ 
+        uvi.textContent="UVI: " + data.current.uvi
+        if (data.current.uvi<=2.00){ 
             uvi.setAttribute(
             "style", "background-color: green"
             )
-        }else if(uvi>=5.00){
+        }else if(data.current.uvi>=5.00){
             uvi.setAttribute(
             "style", "background-color: red" 
             )   
@@ -72,6 +148,8 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city_Name + "&appid
         wForecast[i].textContent= "Wind: " + data.daily[i+1].wind_speed;
         hForecast[i].textContent = "Humidity: " + data.daily[i+1].humidity;
     }
+    
+    })
     cityTitle.textContent=cityInputs.value
     currentDate.textContent=moment().format("L");
     dayOne.textContent=moment().day(1).format("L");
@@ -79,7 +157,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city_Name + "&appid
     dayThree.textContent=moment().day(3).format("L");
     dayFour.textContent=moment().day(4).format("L");
     dayFive.textContent=moment().day(5).format("L");
-    })
+
 });
 }
 weatherRequest(cityName.placeholder)
